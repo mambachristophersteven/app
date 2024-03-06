05971be2d316c0d46c5d9b39f47f32ab2d016438
Christopher Steven <christopherstevennunoo@gmail.com>
2024-03-06 18:59:36 +0000

    removing loader from code once loader completes

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ window.addEventListener('load', ()=>{
     const loader = document.querySelector(".loader");
 
      loader.classList.add("loader-hidden");
+
+
+
+     loader.addEventListener('transitionend', ()=>{
+        document.body.removeChild('loader');
+     })
 })
